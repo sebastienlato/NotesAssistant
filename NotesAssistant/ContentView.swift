879e4b002b5bf6ee83b887fa984e3642e36b3fb6
Sebastien <sebastienlato@gmail.com>
2025-11-29 13:25:20 -0500

    Initial Commit

---
+++ NotesAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesAssistant
+//
+//  Created by sebastien lato on 2025-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesAssistant/NotesAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAssistantApp.swift
+//  NotesAssistant
+//
+//  Created by sebastien lato on 2025-11-29.
+//
+
+import SwiftUI
+
+@main
+struct NotesAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
